5bb5d665a66976023c3992dc084f1ac6b1b0ad8d
cdilga <c.dilger1@gmail.com>
2018-04-24 22:08:15 +1000

    Update

--- scripts/sae-vis-derive.R
+++ scripts/sae-vis-derive.R
@@ -9,14 +9,14 @@ View(dataset)
 typeof(dataset)
 
 datavis <- dataset[c("RMS1_D2_Motor_Speed", 
-                                           "RMS2_D2_Motor_Speed", 
-                                           "RMS1_D3_Motor_Temperature", 
-                                           "RMS2_D3_Motor_Temperature", 
-                                           "RMS1_D2_Torque_Feedback",
-                                           "RMS2_D2_Torque_Feedback",
-                                           "Potentiometer_1",
-                                           "Potentiometer_2",
-                                           "Brake_1" )]
+                     "RMS2_D2_Motor_Speed", 
+                     "RMS1_D3_Motor_Temperature", 
+                     "RMS2_D3_Motor_Temperature", 
+                     "RMS1_D2_Torque_Feedback",
+                     "RMS2_D2_Torque_Feedback",
+                     "Potentiometer_1",
+                     "Potentiometer_2",
+                     "Brake_1" )]
 
 derive <- function(data) {
   # Loop through everything except for the first and last
@@ -28,8 +28,8 @@ derive <- function(data) {
   return(retn)
 }
 
-debug.vis <- function(data, interval = c(120, 180)) {
-  qplot(c(1:length(data)), data, geom="path")
+debug.vis <- function(data) {
+  qplot(c(1:length(data)), data, geom="path", main=sys.call())
 }
 
 # Wheel radius = r (units in metres)
@@ -41,28 +41,38 @@ ratio <- 2.33
 # Mass of the car
 mass <- 86+156
 
-datavis["Motor1_Velocity"] <- (dataset["RMS1_D2_Motor_Speed"]*2*pi*r)/(ratio*60)
-datavis["Motor2_Velocity"] <- (dataset["RMS2_D2_Motor_Speed"]*2*pi*r)/(ratio*60)
+datavis$ID <- 1:nrow(datavis)
+datavis$Motor1_Velocity <- (dataset$RMS1_D2_Motor_Speed*2*pi*r)/(ratio*60)
+datavis$Motor2_Velocity <- (dataset$RMS2_D2_Motor_Speed*2*pi*r)/(ratio*60)
 
-velocityvec1 <- datavis$Motor1_Velocity
-velocityvec2 <- datavis$Motor2_Velocity
+smoothVec <- function(vec, index) {
+  return(
+    predict(loess(vec ~ index, span=0.2))
+  )
+}
 
-velocity <- predict(smooth.spline(datavis$Motor1_Velocity))$y
-velocity.loess <- loess.smooth(
-  c(1:length(datavis$Motor1_Velocity)),
-  datavis$Motor1_Velocity, evaluation=length(datavis$Motor1_Velocity))$y
-
+?predict
 ?loess.smooth()
-?loess.smooth
-datavis$Motor1_Velocity
-length(datavis$Motor1_Velocity)
-length(velocityvec1)
-length(velocity.loess)
-length(acceleration1.loess)
+?loess
+# datavis$Motor1_Velocity_Smooth <- predict(smooth.spline(datavis$Motor1_Velocity))$y
+datavis$Motor1_Velocity_Smooth <- smoothVec(datavis$Motor1_Velocity, datavis$ID)
+datavis$Motor2_Velocity_Smooth <- smoothVec(datavis$Motor2_Velocity, datavis$ID)
 
-acceleration1.loess <- loess.smooth(
-  c(1:length(derive(velocity.loess))),
-  derive(velocity.loess), evaluate=length(derive(velocity.loess)))$y
+datavis$Motor1_Acceleration <- derive(datavis$Motor1_Velocity_Smooth)
+datavis$Motor2_Acceleration <- derive(datavis$Motor2_Velocity_Smooth)
+
+debug.vis(datavis$Motor1_Velocity - datavis$Motor1_Velocity_Smooth)
+
+#This shouldn't be a flat line.
+debug.vis(datavis$Motor1_Acceleration - derive(datavis$Motor1_Velocity))
+
+datavis$Motor1_Acceleration_Smooth <- smoothVec(datavis$Motor1_Acceleration, datavis$ID)
+datavis$Motor2_Acceleration_Smooth <- smoothVec(datavis$Motor2_Acceleration, datavis$ID)
+
+debug.vis(datavis$Motor1_Acceleration)
+#acceleration1.loess <- loess.smooth(
+#  c(1:length(derive(velocity.loess))),
+#  derive(velocity.loess), evaluate=length(derive(velocity.loess)))$y
 
 head(velocity.loess$y)
 
